e108c2748e44425f371959cb0528773ede94b446
bereka03 <giorgi.berekashvili.1@btu.edu.ge>
2023-10-27 16:16:15 +0400

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.TransformerException;
+
+public class Main {
+    public static void main(String[] args) throws ParserConfigurationException, TransformerException {
+        XMLUtils.createXML();
+    }
+}
---
+++ src/XMLUtils.java
@@ -0,0 +1,60 @@
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
+import javax.xml.crypto.dsig.Transform;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import java.io.File;
+
+public class XMLUtils  {
+
+    public static void createXML() throws ParserConfigurationException, TransformerException {
+        DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
+        Document document = documentBuilder.newDocument();
+
+        Element rootElement = document.createElement("pack");
+        document.appendChild(rootElement);
+
+        Element letters = document.createElement("letters");
+        rootElement.appendChild(letters);
+
+        addLetterElement(document, letters, "ABC00001", "Recepient 1", "Somewhere NSW 2345");
+        addLetterElement(document, letters, "DEF00002", "Recepient 2", "Somewhere NSW 2345");
+        addLetterElement(document, letters, "GHI00003", "Recepient 3", "Somewhere NSW 2345");
+
+        TransformerFactory transformerFactory = TransformerFactory.newInstance();
+        Transformer transformer = transformerFactory.newTransformer();
+        DOMSource domSource = new DOMSource(document);
+        StreamResult streamResult = new StreamResult(new File("C:/Users/user/IdeaProjects/quiz2/src/a.xml"));
+
+        transformer.transform(domSource, streamResult);
+    }
+
+    private static void addLetterElement(Document document, Element parentElement, String reference, String line, String line2) {
+        Element letterElement = document.createElement("letter");
+        parentElement.appendChild(letterElement);
+        
+        Element postalElement = document.createElement("postal");
+        letterElement.appendChild(postalElement);
+
+        Element referenceElement = document.createElement("reference");
+        referenceElement.appendChild(document.createTextNode(reference));
+        letterElement.appendChild(referenceElement);
+
+        Element lineElement1 = document.createElement("line");
+        lineElement1.appendChild(document.createTextNode(line));
+        postalElement.appendChild(lineElement1);
+
+        Element lineElement2 = document.createElement("line");
+        lineElement2.appendChild(document.createTextNode(line2));
+        postalElement.appendChild(lineElement2);
+    }
+}
